708788c6959bed4dad9cc4500d42ed30cee5cf65
Anam Rani <Anam Rani>
2022-09-17 19:20:34 +0500

    Array si manipulated

--- Frineds Names/script.js
+++ Frineds Names/script.js
@@ -1,5 +1,11 @@
 // array of frind List
 let friendsNames = ['Mam Sadia', 'Maham', 'Noor', 'fariha', 'Mam Muniba'];
+console.log(friendsNames[4] + " Can't make dinner");
+friendsNames[4] = "Mam Hina";
+console.log("you found a bigger dinner table.");
+friendsNames.unshift("Zunaira");
+friendsNames.splice(3, 0 , "Ayehsa");
+friendsNames.push("sibgha");
 for ( var i =0; i < friendsNames.length; i ++) {
-   console.log("Hello, I'm invitin  you for lunch  " + friendsNames[i]);
+   console.log("Hello, I'm invitin  you for Dinner  " + friendsNames[i]);
 }
