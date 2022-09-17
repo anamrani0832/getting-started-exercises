50806363b48775ce13d4add056805a2f7b7634e2
Anam Rani <Anam Rani>
2022-09-17 17:52:01 +0500

    Array is implimented

---
+++ Frineds Names/script.js
@@ -0,0 +1,5 @@
+// array of frind List
+let friendsNames = ['Mam Sadia', 'Maham', 'Noor', 'fariha', 'Mam Muniba'];
+for ( var i =0; i < friendsNames.length; i ++) {
+   console.log(friendsNames[i]);
+}
