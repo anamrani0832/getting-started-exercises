5f3932ad97fddcab904cf411a0a42f53f79a4f0c
Anam Rani <Anam Rani>
2022-09-28 19:27:19 +0500

    all changes are made

---
+++ 20/index.js
@@ -0,0 +1,3 @@
+let myArray = [120, false, 'Anam', 400];
+delete myArray[1];
+console.log(myArray);
--- Frineds Names/script.js
+++ Frineds Names/script.js
@@ -7,19 +7,4 @@ friendsNames.unshift("Zunaira");
 friendsNames.splice(3, 0 , "Ayehsa");
 friendsNames.push("sibgha");
 console.log("I can invite only two people for dinner");
-for ( i = 0; i <=5; i++) {
-    console.log(`${friendsNames[i]} you’re sorry I can’t invite you to dinner`);
-    friendsNames.pop(i)
-}
-for ( var i =0; i < friendsNames.length; i ++) {
-   console.log("Hello, I'm invitin  you for Dinner  " + friendsNames[i]);
-}
-console.log("I can invite only two people for dinner.");
-for ( var i =0; i <= 2; i ++) {
-   console.log(`you’re sorry I can’t invite you ${friendsNames[i]} to dinner`);
-   friendsNames.pop(friendsNames[i])
- }
- for ( var i =0; i <= friendsNames.length; i ++) {
-    console.log(` you're  still invited ${friendsNames[i]} to dinner`);
-  }
-  console.log(friendsNames.length)
+console.log(friendsNames);
