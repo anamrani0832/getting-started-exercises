28ef9c469592ee1ded7d9207b043502fced19fa2
Anam Rani <Anam Rani>
2022-09-29 06:52:27 +0500

    all changes are made

--- 20/index.js
+++ 20/index.js
@@ -1,3 +1,11 @@
-let myArray = [120, false, 'Anam', 400];
-delete myArray[1];
+let myArray = [120, false, 'Anam',400];
 console.log(myArray);
+let popedElements = []
+for ( i =0; i < myArray.length; i ++) {
+    if (myArray.length > 2) {
+        popedElements.push(myArray.pop(i)); 
+    }
+}
+console.log("popped elements  : " + popedElements);
+console.log( "Remaining elemts of array : " + myArray);
+
---
+++ Conditional Tests/script.js
@@ -0,0 +1,16 @@
+// let cars = ['audi', 'bmw', 'subaru', 'toyota'];
+// for(let i =0; i < cars.length; i++) {
+// if (cars[i] == "bmw" ) {
+//     alert(cars[i].toUpperCase());
+// }
+// else {
+//     alert(cars[i]);
+// }
+// }
+ let car = 'Audi';
+ alert(car.toLowerCase() == "audi");
+ let num = 18;
+ alert( num == 18);
+if ( num != 56) {
+    alert("FAIL");
+}
