ca904fb6875eb1d3e192711e00f09a361c21151d
Anam Rani <Anam Rani>
2022-10-03 03:01:31 +0500

    all changes are made

---
+++ City Names/script.js
@@ -0,0 +1,8 @@
+function city_country( city , country = "Pakistan") {
+    let msg = `"${city} , ${country}"`;
+    return alert(msg);
+}
+
+city_country( "Farscisco", "Landon");
+city_country("Lahore", "Pakistan");
+city_country("Karachi", "Pakistan");
