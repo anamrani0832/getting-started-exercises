eb970fb631220d5eee2f735fb3d98c3853f3311a
Anam Rani <Anam Rani>
2022-10-03 02:47:37 +0500

    all changes are made

---
+++ T-Shirt/script.js
@@ -0,0 +1,8 @@
+function make_shirt( size = "large", meassage = "i love javascript") {
+
+return console.log(`The Shirt size will be ${size} and The message will be ${meassage}`);
+
+}
+make_shirt()
+make_shirt(size='medium')
+make_shirt('small', 'Programmers are loopy.')
