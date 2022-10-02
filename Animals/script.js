b4ec6632f561055347147b214e0cd6809f59da6c
Anam Rani <Anam Rani>
2022-10-03 02:34:06 +0500

    all changes are made

---
+++ Animals/script.js
@@ -0,0 +1,5 @@
+let animals = ['horse', 'cow', 'elephant'];
+for ( let animal of animals) {
+    console.log(animal + "would make a great pet");
+}
+console.log("Any of these animals would make a great pet!");
---
+++ Pizzas/script.js
@@ -0,0 +1,5 @@
+let Pizzas = ["p1", "p2" , "p3"];
+for (let p of Pizzas) {
+    console.log(`ilke ${p} pizza`);
+}
+console.log("I really love pizza!");
