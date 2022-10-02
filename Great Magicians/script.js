86ee04621e5917f362f0cfa750a73cc882e4f27b
Anam Rani <Anam Rani>
2022-10-03 04:08:51 +0500

    all changes are made

--- Great Magicians/script.js
+++ Great Magicians/script.js
@@ -4,15 +4,6 @@ for ( let magic of magicians)
    console.log(magic);
 }
 }
-function make_great(magicians) {
-for (let i=0; i < magicians.length; i ++)
-{
-   magicians[i] += " the great";
-}
-
-}
 
 let magiciArray = ['Anam', 'Maham', 'Noor'];
-show_magicians(magiciArray);
-make_great(magiciArray);
 show_magicians(magiciArray);
